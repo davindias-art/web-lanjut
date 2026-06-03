118389264f7c8498155714821bd3d684f646cfe7
davindias-art <davindias06@gmail.com>
2026-06-03 11:03:46 +0700

    Update index.js
    Penambahan app.get('/',(req,res)=>{
    res.json({message:"Hello coba backend untuk vercel"});
    }); agar saat berhasil menjalankan/menghubungkan vercel dengan github muncul pesan tersebut

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ try {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.get('/',(req,res)=>{
+res.json({message:"Hello coba backend untuk vercel"});
+});
 app.use('/api/buku', router);
 app.use('/api/mahasiswa',mahasiswas);
 app.use('/api/prodi',prodis);
