88f324dbcc2354b7e7084d319a89e92c3aea84f1
davindias-art <davindias06@gmail.com>
2026-06-03 11:47:33 +0700

    Update db.config.js

--- config/db.config.js
+++ config/db.config.js
@@ -3,6 +3,8 @@ import mysql2 from "mysql2";
 const db = new Sequelize ('defaultdb', 'avnadmin', 'AVNS_Jd5Bx5Bgsk56hHoW9kf', {
     host: "mysql-7793bcc-web-lanjut.h.aivencloud.com",
     dialect: "mysql",
+    port: "16602",
+    dialectOptions: { ssl: { rejectUnauthorized: false } }, 
     "define": {
         "timestamps": false
     }
