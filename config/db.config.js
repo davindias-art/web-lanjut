21521122f33cf126bc7d8e51f1873cf1c5586396
davindias-art <davindias06@gmail.com>
2026-06-10 01:01:02 +0700

    Penyesuaian terhadap database Aiven, agar saling terhubung satu sama lain. Juga Kita menambahkan import mysql2 agar berhasil jalan dan terhubung pada vercel

--- config/db.config.js
+++ config/db.config.js
@@ -1,13 +1,20 @@
 import { Sequelize } from "sequelize";
-const db = new Sequelize ('web_lanjut', 'root', '', {
-    host: "localhost",
+import mysql2 from "mysql2";
+const db = new Sequelize ('defaultdb', 'avnadmin', 'AVNS_Jd5Bx5Bgsk56hHoW9kf', {
+    host: "mysql-7793bcc-web-lanjut.h.aivencloud.com",
     dialect: "mysql",
+    port: "16602",
+    dialectOptions: { 
+        ssl: { 
+            rejectUnauthorized: false 
+        }
+    }, 
     "define": {
         "timestamps": false
     }
 });
 export default db;
 
-(async()=>{
-    await db.sync();
-}) ();
+// (async()=>{
+//     await db.sync();
+// }) ();
